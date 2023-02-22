72ccbacb81a84128076a1b0a97b63a873010be50
Victor Tran <vc.tran998@gmail.com>
2023-02-22 14:55:57 -0800

    Add stats to form

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
   // change null to player name submitted
   handleChange = (event) => {
       const replace = event.target.value.split(" ").join("_");
-      if(replace.length > 0){
+      if(replace.length > -2){
         this.setState({playerName: replace})
       } else {
         alert("Please enter a valid player name")
@@ -31,9 +31,13 @@ class App extends Component {
   getPlayerId = () => {
     axios.get(`https://www.balldontlie.io/api/v1/players?search=${this.state.playerName}`)
     .then(async res => {
-      console.log(res.data.data)
-      if(res.data.data===undefined){
-        alert("This player has not played this season!")
+     // console.log(res.data.data)
+      if(res.data.data[0]=== undefined){
+        alert("Enter a valid player")
+      } else if(res.data.data.length > 1) {
+        alert("Please enter correct name!")
+      } else {
+        await this.getPlayerStats(res.data.data[0].id)
       }
       await this.getPlayerStats(res.data.data[0].id)
     }).catch(err => {
@@ -41,20 +45,17 @@ class App extends Component {
     })
   }
 
-  getPlayerStats = (playerID) => {
-    axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=${playerID}`)
+  getPlayerStats = (playerId) => {
+    axios.get(`https://www.balldontlie.io/api/v1/season_averages?season=2022&player_ids[]=${playerId}`)
     .then(async (res) => {
-      console.log(res.data.data)
+       console.log(res.data.data)
       this.setState({ playerStats: res.data.data[0]})
     }).catch(err => {
       console.log(err)
     })
   }
 
-  componentDidMount(){
-    this.getPlayerId()
-    this.getPlayerStats()
-  }
+
 
   render () {
   return (
@@ -71,6 +72,27 @@ class App extends Component {
           </label>
           <input type="submit" value="Submit"/>
        </form>
+       games played: {this.state.playerStats["games_played"]}
+       <br />
+       points averaged: {this.state.playerStats["pts"]}
+       <br />
+       rebounds averaged: {this.state.playerStats["reb"]}
+       <br />
+       assists averaged: {this.state.playerStats["ast"]}
+       <br />
+       blocks averaged: {this.state.playerStats["blk"]}
+       <br />
+       FG%: {this.state.playerStats["fg_pct"]*100}
+       <br />
+      3pt%: {this.state.playerStats["fg3_pct"]*100}
+      <br />
+      minutes averaged: {this.state.playerStats["min"]}
+      <br />
+      steals averaged: {this.state.playerStats["stl"]}
+      <br />
+      turnover averaged: {this.state.playerStats["turnover"]}
+      <br />
+      FT%: {this.state.playerStats["ft_pct"]*100}
     </div>
   );
 }
