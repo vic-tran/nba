e33be5c52221973e7aa39ead9c465f6a9b89362b
Victor Tran <vc.tran998@gmail.com>
2023-02-22 12:03:16 -0800

    Import balldontlie API + add get player id function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
 import React, {Component} from "react";
+import axios from "axios";
+
 
 class App extends Component {
+    constructor (props) {
+      super(props)
+      this.state = {
+        playerName: null,
+        playerStats: {}
+    }
+  }
+
+  getPlayerId = () => {
+    axios.get("https://www.balldontlie.io/api/v1/players?search=kyrie")
+    .then(async res => {
+      console.log(res.data.data)
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  componentDidMount(){
+    this.getPlayerId()
+  }
+
   render () {
   return (
     <div className="App">
