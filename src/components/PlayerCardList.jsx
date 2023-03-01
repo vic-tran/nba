c503818d2fdbeabb3f83873326c23d747ad06d37
Victor Tran <vc.tran998@gmail.com>
2023-03-01 13:49:11 -0800

    Styling for playerlist

--- src/components/PlayerCardList.jsx
+++ src/components/PlayerCardList.jsx
@@ -1,39 +1,45 @@
 import React, { Component } from 'react';
 import PlayerCard from './PlayerCard';
 
+
 class PlayerCardList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      players: [],
-    };
-  }
-
-  addPlayer = () => {
-    const { players } = this.state;
-    this.setState({ players: [...players, {}] });
-  };
-
-  removePlayer = (index) => {
-    const { players } = this.state;
-    players.splice(index, 1);
-    this.setState({ players });
-  };
-
-  render() {
-    const { players } = this.state;
-    return (
-      <div>
-        <button onClick={this.addPlayer}>Add Player</button>
-        {players.map((player, index) => (
-          <div key={index}>
-            <PlayerCard />
-            <button onClick={() => this.removePlayer(index)}>Remove Player</button>
+    constructor(props) {
+        super(props);
+        this.state = {
+          players: []
+        };
+      }
+    
+      addPlayer = () => {
+        const newPlayer = <PlayerCard />;
+        this.setState(prevState => ({
+          players: [...prevState.players, newPlayer]
+        }));
+      };
+    
+      removePlayer = () => {
+        this.setState(prevState => ({
+          players: prevState.players.slice(0, -1)
+        }));
+      };
+    
+      render() {
+        const { players } = this.state;
+        return (
+          <div>
+            <div>
+              <button onClick={this.addPlayer}>Add Player</button>
+              <button onClick={this.removePlayer}>Remove Player</button>
+            </div>
+            <div style={{ display: "flex" }}>
+              {players.map((player, index) => (
+                <div key={index} style={{ display: "inline-block" }}>
+                  {player}
+                </div>
+              ))}
+            </div>
           </div>
-        ))}
-      </div>
-    );
-  }
-}
-
+        );
+      }
+    }
 export default PlayerCardList;
