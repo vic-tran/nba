708bba5794c98ac5d945ff05852e105561d80508
Victor Tran <vc.tran998@gmail.com>
2023-03-08 10:27:30 -0800

    Style changes on background and playercard

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Home from "./components/Home";
 function App() {
   return (
     <html>
-      <div className="app">
+      <div className="app"
+       style ={{ backgroundImage: `url("https://i.pinimg.com/originals/65/f2/5a/65f25aa693bf16f241345be7edd44614.jpg")`}}
+      >
         <Home />
       </div>
     </html>
